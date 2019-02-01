5801320c49f20e348323db02a67f795b33649236
cohyunoh <coh00@stuy.edu>
2019-01-31 23:48:21 -0500

    trying to test method realized i need my tolerance parameter to test

---
+++ driver.java
@@ -0,0 +1,7 @@
+public class driver{
+  public static void main(String[] args) {
+    System.out.println(recursion.sqrt(4.0));
+    System.out.println(recursion.sqrt(100.0));
+    System.out.println(recursion.sqrt(2.0));
+  }
+}
