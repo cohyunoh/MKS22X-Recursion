20f0df1e303718941db183312a919d44eccf6c60
cohyunoh <coh00@stuy.edu>
2019-01-31 23:49:38 -0500

    test a success

--- driver.java
+++ driver.java
@@ -1,7 +1,9 @@
 public class driver{
   public static void main(String[] args) {
+    //sqrt test 1
     System.out.println(recursion.sqrt(4.0, 0.000001));
     System.out.println(recursion.sqrt(100.0, 0.000001));
     System.out.println(recursion.sqrt(2.0, 0.000001));
+    //test 1 works
   }
 }
