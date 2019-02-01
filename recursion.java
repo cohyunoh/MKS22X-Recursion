7c6425b3c68a4734c1bfcff051f108f6738df08b
cohyunoh <coh00@stuy.edu>
2019-01-31 23:54:40 -0500

    added comments to sqrt methods

--- recursion.java
+++ recursion.java
@@ -3,11 +3,18 @@ public class recursion{
     return recursion.sqrtHelper(n, 1.0, tolerance);
   }
   private static double sqrtHelper(double n, double guess, double tolerance){
+    // this stores the new guess using Newton's Square Root Approximation Equation
     double newGuess = (n / guess + guess) / 2.0;
+    // this stores how accurate the answer was by using percent error
     double accurate = (Math.abs(n - (guess * guess))) / n;
+    // if the stored accurate value is greater than the tolerance then it will do the
+    // recursive case
     if (accurate > tolerance){
+    // only thing changing is that we replace guess with newGuess
       return sqrtHelper(n, newGuess, tolerance);
     }else{
+    // otherwise it means that it passes the tolerance and it will return the new new
+    // guess (BASE CASE)
       return newGuess;
     }
   }
