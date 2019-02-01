de97321320e332ec4e03d7bb6066882f5bc1508f
cohyunoh <coh00@stuy.edu>
2019-01-31 23:48:47 -0500

    driver compiled

--- driver.java
+++ driver.java
@@ -1,7 +1,7 @@
 public class driver{
   public static void main(String[] args) {
-    System.out.println(recursion.sqrt(4.0));
-    System.out.println(recursion.sqrt(100.0));
-    System.out.println(recursion.sqrt(2.0));
+    System.out.println(recursion.sqrt(4.0, 0.000001));
+    System.out.println(recursion.sqrt(100.0, 0.000001));
+    System.out.println(recursion.sqrt(2.0, 0.000001));
   }
 }
