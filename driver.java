444193fadb9fe0f0104a83e3db788e52df912849
cohyunoh <coh00@stuy.edu>
2019-02-03 10:16:50 -0500

    All tests were successful

--- driver.java
+++ driver.java
@@ -4,8 +4,26 @@ public class driver{
     System.out.println(recursion.sqrt(4.0, 0.000001));
     System.out.println(recursion.sqrt(100.0, 0.000001));
     System.out.println(recursion.sqrt(2.0, 0.000001));
+    //test 1 works
+    //makeAllSums test 2
     System.out.println(recursion.makeAllSums(3));
     System.out.println(recursion.makeAllSums(5));
-    //test 1 works
+    System.out.println(recursion.makeAllSums(10));
+    System.out.println(recursion.makeAllSums(20));
+    //test 2 works
+    //fib test 3
+    System.out.println(recursion.fib(0));
+    System.out.println(recursion.fib(1));
+    System.out.println(recursion.fib(2));
+    System.out.println(recursion.fib(3));
+    System.out.println(recursion.fib(4));
+    System.out.println(recursion.fib(5));
+    System.out.println(recursion.fib(6));
+    System.out.println(recursion.fib(7));
+    System.out.println(recursion.fib(8));
+    System.out.println(recursion.fib(9));
+    System.out.println(recursion.fib(10));
+    System.out.println(recursion.fib(11));
+    //test 3 works
   }
 }
