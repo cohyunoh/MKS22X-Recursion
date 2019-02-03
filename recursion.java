e3ca51d7dd5adac0e2ef7152f8a4c01009672d9f
cohyunoh <coh00@stuy.edu>
2019-02-03 10:03:42 -0500

    added  comments for future refrence to these methods

--- recursion.java
+++ recursion.java
@@ -21,15 +21,27 @@ public class recursion{
   }
 
   public static ArrayList<Integer> makeAllSums(int n){
+    // makes an ArrayList that will be populated with all the sums
     ArrayList<Integer> ans = new ArrayList<Integer>();
+    // it then calls the helper method and will run until the method is done
     recursion.makeAllSumsHelper(n, 0, ans);
+    // once done, it will return the ArrayList
     return ans;
   }
-
+//VISUAL REPRESENTATION
+//                             h(3,0,L)
+//                          /           \
+//                  h(2,3,L)              h(2,0,L)
+//               /        \              /         \
+//        h(1,5,L)        h(1,3,L)      h(1,2,L)       h(1,0,L)
+//        /     \         /      \       /     \       /      \
+//  h(0,6,L) h(0,5,L) h(0,4,L) h(0,3,L)*2  h(0,2,L) h(0,1,L) h(0,0,L)
   public static void makeAllSumsHelper(int n, int i, ArrayList<Integer> L){
+    // Once n is 0 it will add whatever the i value is
     if(n == 0){
       L.add(i);
     }else{
+    // otherwise it will do the recursive call which calls itself twice for both cases
       makeAllSumsHelper(n - 1, i, L);
       makeAllSumsHelper(n - 1, i + n, L);
     }
